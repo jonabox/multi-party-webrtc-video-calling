7e167b3f81284d8c73aa7f316ecca988ef76b13f
Jonathan Esteban <jesteban@mit.edu>
2020-06-09 01:21:52 -0400

    added simple chatserver

--- chatserver.js
+++ chatserver.js
@@ -1,4 +1,5 @@
 //#!/usr/bin/env node
+// adapted from https://github.com/mdn/samples-server/tree/master/s/websocket-chat
 
 //
 // WebSocket chat server
@@ -21,8 +22,8 @@ var appendToMakeUnique = 1;
 // server.
 
 var httpsOptions = {
-  key: fs.readFileSync("/etc/pki/tls/private/mdn-samples.mozilla.org.key"),
-  cert: fs.readFileSync("/etc/pki/tls/certs/mdn-samples.mozilla.org.crt")
+  key: fs.readFileSync("/etc/pki/tls/private/server.key"),
+  cert: fs.readFileSync("/etc/pki/tls/certs/server.cert")
 };
 
 // Our HTTPS server does nothing but service WebSocket
@@ -30,22 +31,22 @@ var httpsOptions = {
 // requests are handled by the main server on the box. If you
 // want to, you can return real HTML here and serve Web content.
 
-var httpsServer = https.createServer(httpsOptions, function(request, response) {
-    console.log((new Date()) + " Received request for " + request.url);
-    response.writeHead(404);
-    response.end();
+var httpsServer = https.createServer(httpsOptions, function (request, response) {
+  console.log((new Date()) + " Received request for " + request.url);
+  response.writeHead(404);
+  response.end();
 });
 
-httpsServer.listen(6502, function() {
-    console.log((new Date()) + " Server is listening on port 6502");
+httpsServer.listen(6502, function () {
+  console.log((new Date()) + " Server is listening on port 6502");
 });
 
 // Create the WebSocket server
 
 console.log("***CREATING WEBSOCKET SERVER");
 var wsServer = new WebSocketServer({
-    httpServer: httpsServer,
-    autoAcceptConnections: false
+  httpServer: httpsServer,
+  autoAcceptConnections: false
 });
 console.log("***CREATED");
 
@@ -59,7 +60,7 @@ function isUsernameUnique(name) {
   var isUnique = true;
   var i;
 
-  for (i=0; i<connectionArray.length; i++) {
+  for (i = 0; i < connectionArray.length; i++) {
     if (connectionArray[i].username === name) {
       isUnique = false;
       break;
@@ -72,7 +73,7 @@ function getConnectionForID(id) {
   var connect = null;
   var i;
 
-  for (i=0; i<connectionArray.length; i++) {
+  for (i = 0; i < connectionArray.length; i++) {
     if (connectionArray[i].clientID === id) {
       connect = connectionArray[i];
       break;
@@ -91,7 +92,7 @@ function makeUserListMessage() {
 
   // Add the users to the list
 
-  for (i=0; i<connectionArray.length; i++) {
+  for (i = 0; i < connectionArray.length; i++) {
     userListMsg.users.push(connectionArray[i].username);
   }
 
@@ -103,20 +104,32 @@ function sendUserListToAll() {
   var userListMsgStr = JSON.stringify(userListMsg);
   var i;
 
-  for (i=0; i<connectionArray.length; i++) {
+  for (i = 0; i < connectionArray.length; i++) {
     connectionArray[i].sendUTF(userListMsgStr);
   }
 }
 
+function sendToOneUser(target, msgString) {
+  var isUnique = true;
+  var i;
+
+  for (i = 0; i < connectionArray.length; i++) {
+    if (connectionArray[i].username === target) {
+      connectionArray[i].send(msgString);
+      break;
+    }
+  }
+}
+
 console.log("***CRETING REQUEST HANDLER");
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
   console.log("Handling request from " + request.origin);
   if (!originIsAllowed(request.origin)) {
     request.reject();
     console.log("Connection from " + request.origin + " rejected.");
     return;
   }
-  
+
   // Accept the request and get a connection.
 
   var connection = request.accept("json", request.origin);
@@ -142,76 +155,80 @@ wsServer.on('request', function(request) {
   // is a message sent by a client, and may be text to share with
   // other users or a command to the server.
 
-  connection.on('message', function(message) {
-      console.log("***MESSAGE");
-      if (message.type === 'utf8') {
-          console.log("Received Message: " + message.utf8Data);
+  connection.on('message', function (message) {
+    console.log("***MESSAGE");
+    if (message.type === 'utf8') {
+      console.log("Received Message: " + message.utf8Data);
 
-          // Process messages
+      // Process messages
 
-          var sendToClients = true;
-          msg = JSON.parse(message.utf8Data);
-          var connect = getConnectionForID(msg.id);
+      var sendToClients = true;
+      msg = JSON.parse(message.utf8Data);
+      var connect = getConnectionForID(msg.id);
 
-          // Look at the received message type and
-          // handle it appropriately.
+      // Look at the received message type and
+      // handle it appropriately.
 
-          switch(msg.type) {
-            // Public text message in the chat room
-            case "message":
-              msg.name = connect.username;
-              msg.text = msg.text.replace(/(<([^>]+)>)/ig,"");
-              break;
+      switch (msg.type) {
+        // Public text message in the chat room
+        case "message":
+          msg.name = connect.username;
+          msg.text = msg.text.replace(/(<([^>]+)>)/ig, "");
+          break;
 
-            // Username change request
-            case "username":
-              var nameChanged = false;
-              var origName = msg.name;
+        // Username change request
+        case "username":
+          var nameChanged = false;
+          var origName = msg.name;
 
-              // Force a unique username by appending
-              // increasing digits until it's unique.
-              while (!isUsernameUnique(msg.name)) {
-                msg.name = origName + appendToMakeUnique;
-                appendToMakeUnique++;
-                nameChanged = true;
-              }
-
-              // If the name had to be changed, reject the
-              // original username and let the other user
-              // know their revised name.
-              if (nameChanged) {
-                var changeMsg = {
-                  id: msg.id,
-                  type: "rejectusername",
-                  name: msg.name
-                };
-                connect.sendUTF(JSON.stringify(changeMsg));
-              }
-
-              connect.username = msg.name;
-              sendUserListToAll();
-              break;
+          // Force a unique username by appending
+          // increasing digits until it's unique.
+          while (!isUsernameUnique(msg.name)) {
+            msg.name = origName + appendToMakeUnique;
+            appendToMakeUnique++;
+            nameChanged = true;
           }
 
-          // Convert the message back to JSON and send it out
-          // to all clients.
-
-          if (sendToClients) {
-            var msgString = JSON.stringify(msg);
-            var i;
-
-            for (i=0; i<connectionArray.length; i++) {
-              connectionArray[i].sendUTF(msgString);
-            }
+          // If the name had to be changed, reject the
+          // original username and let the other user
+          // know their revised name.
+          if (nameChanged) {
+            var changeMsg = {
+              id: msg.id,
+              type: "rejectusername",
+              name: msg.name
+            };
+            connect.sendUTF(JSON.stringify(changeMsg));
           }
+
+          connect.username = msg.name;
+          sendUserListToAll();
+          break;
       }
+
+      // Convert the message back to JSON and send it out
+      // to all clients.
+
+      if (sendToClients) {
+        var msgString = JSON.stringify(msg);
+        var i;
+
+        if (msg.target && msg.target !== undefined && msg.target.length !== 0) {
+          sendToOneUser(msg.target, msgString);
+        } else {
+          for (i = 0; i < connectionArray.length; i++) {
+            connectionArray[i].send(msgString);
+          }
+        }
+      }
+    }
   });
-  
+
   // Handle the WebSocket "close" event; this means a user has logged off
   // or has been disconnected.
-  
-  connection.on('close', function(connection) {
-    connectionArray = connectionArray.filter(function(el, idx, ar) {
+
+  connection.on('close', function (connection) {
+    connectionArray = connectionArray.filter(function (el, idx, ar) {
       return el.connected;
     });
     sendUserListToAll();  // Update the user lists
